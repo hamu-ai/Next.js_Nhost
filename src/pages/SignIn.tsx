5f2659d35ba2ee92a9d2c4c5fee23c3c7a32dec0
mahi kisimoto <km0227km@gmail.com>
2022-10-18 22:02:48 +0900

    ⚡️ サインインページ

---
+++ src/pages/SignIn.tsx
@@ -0,0 +1,12 @@
+import { NextPage } from "next";
+import SignIn from "src/components/SignIn";
+
+const SignInPage: NextPage = () => {
+  return (
+    <div>
+      <SignIn />
+    </div>
+  );
+};
+
+export default SignInPage;
